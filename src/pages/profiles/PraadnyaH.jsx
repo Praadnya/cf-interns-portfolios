a2309fe7de82208781776b7b129ad2fb90acd3a9
Praadnya <hpraadnya@gmail.com>
2025-02-06 15:58:19 +0530

    Initial commit

---
+++ src/pages/profiles/PraadnyaH.jsx
@@ -0,0 +1,25 @@
+import { Button } from "../../components/ui/button"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+  
+
+export default function PraadnyaH() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+
+    </div>
+  )
+}
