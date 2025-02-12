b92e9ede8fbb60481601baec62774339284aee2f
avdhagam <davanisri@gmail.com>
2025-02-12 13:18:30 +0530

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import PraadnyaH from "./pages/profiles/PraadnyaH";
+import Avani from "./pages/profiles/Avani";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes> 
+      <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/avani" element={<Avani />}></Route>
         <Route path="/praadnya-h" element={<PraadnyaH />}></Route>
       </Routes>
     </BrowserRouter>
